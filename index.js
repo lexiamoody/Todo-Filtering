892933f5f851ecdb028260653bde9dc29871bdfd
lexiamoody <60360405+lexiamoody@users.noreply.github.com>
2020-11-09 12:59:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
-
-   let arrayOfTodos = [
+let arrayOfTodos = [
     {
-    "userId": 14,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 20,
-    "id": 2,
-    "title": "delectus aut autem",
-    "completed": false
-  }
-  {
-    "userId": 14,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 20,
-    "id": 2,
-    "title": "delectus aut autem",
-    "completed": false
-  }]
+        "userId": 14,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    },
+    {
+        "userId": 20,
+        "id": 2,
+        "title": "delectus aut autem",
+        "completed": false
+    }]
 
-  const fetchTodos = () => {
+const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
-    .then( (response) => response.json())
-    .then( (json) => arrayOfTodos = json)
-  }
+        .then((response) => response.json())
+        .then((json) => arrayOfTodos = json)
+}
 
-  const logTodos = () => {
+const logTodos = () => {
     console.log(arrayOfTodos)
-  }
+}
 
-  const populateTodos = () => {
+const populateTodos = () => {
+    
+    let todoList = document.getElementById('todo-list');
 
-  }
+  
+    let todoTitle = "";
+
+    for (i=0; i < arrayOfTodos.length; i++) {
+        todoTitle = document.createTextNode(arrayOfTodos[i].title);
+
+      
+        let todoItem = document.createElement("LI");
+        if (arrayOfTodos[i].completed) {
+           
+            todoItem.classList.add("istrue");
+        } else {
+            todoItem.classList.add("isfalse");
+        }
+
+       
+        todoItem.appendChild(todoTitle);
+
+     
+        todoList.appendChild(todoItem);
+    }
+
+  
+    console.log(arrayOfTodos[0].title)
+
+}
