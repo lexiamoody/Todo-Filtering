d3f391a879af424485981d7fa2cdc0140fce3324
lexiamoody <60360405+lexiamoody@users.noreply.github.com>
2020-11-09 12:54:43 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+   let arrayOfTodos = [
+    {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 20,
+    "id": 2,
+    "title": "delectus aut autem",
+    "completed": false
+  }
+  {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 20,
+    "id": 2,
+    "title": "delectus aut autem",
+    "completed": false
+  }]
+
+  const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+  }
+
+  const logTodos = () => {
+    console.log(arrayOfTodos)
+  }
+
+  const populateTodos = () => {
+
+  }
